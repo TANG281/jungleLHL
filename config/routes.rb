e36a961dc18c0224383171d7a1e777fed2825949
Tuan Anh Nguyen <atngu312@gmail.com>
2023-12-04 00:48:41 -0500

    Feature Admin Categories

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+  
+  namespace :admin do
+    resources :categories, only: [:index, :new, :create]
+  end
 
   get 'about', to: 'about#index'
   # The priority is based upon order of creation: first created -> highest priority.
